21ca8fffbb9e62b260693493ce6e55cf7013653d
AaronLieb <aarnliebmrn@gmail.com>
2020-09-24 16:00:29 -0700

    Added Xcode Project

---
+++ Astraeus/AstraeusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraeusApp.swift
+//  Astraeus
+//
+//  Created by Aaron Lieberman on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct AstraeusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
