066fbde513675c3aa8d6569bca8afea26d030f74
AaronLieb <aarnliebrmn@gmail.com>
2020-10-01 18:05:36 -0700

    created Navigation for SpaceLList

---
+++ Astraeus/SpaceDetail.swift
@@ -0,0 +1,20 @@
+//
+//  SpaceDetail.swift
+//  Astraeus
+//
+//  Created by Aaron Lieberman on 10/1/20.
+//
+
+import SwiftUI
+
+struct SpaceDetail: View {
+    var body: some View {
+        Text("Details")
+    }
+}
+
+struct SpaceDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        SpaceDetail()
+    }
+}
--- Astraeus/SpaceList.swift
+++ Astraeus/SpaceList.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct SpaceList: View {
     var body: some View {
-        List {
-            ListItem(text: "The Sun", image: Image(systemName: "sun.max.fill"))
-            ListItem(text: "Earth", image: Image(systemName: "globe"))
-            ListItem(text: "Mars", image: Image(systemName: "m.circle.fill"))
-            ListItem(text: "Mercury", image: Image(systemName: "m.circle.fill"))
+        NavigationView {
+            List {
+                ListItem(text: "The Sun", image: Image(systemName: "sun.max.fill"))
+                ListItem(text: "Earth", image: Image(systemName: "globe"))
+                ListItem(text: "Mars", image: Image(systemName: "m.circle.fill"))
+                ListItem(text: "Mercury", image: Image(systemName: "m.circle.fill"))
+            }.navigationBarTitle(Text("Planets"))
         }
     }
 }
@@ -22,10 +24,12 @@ struct ListItem: View {
     var text: String
     var image: Image
     var body: some View {
-        HStack {
-            image.padding()
-            Spacer()
-            Text(text).padding()
+        NavigationLink(destination: SpaceDetail()) {
+            HStack {
+                image.padding()
+                Text(text).padding()
+                Spacer()
+            }
         }
     }
 }
