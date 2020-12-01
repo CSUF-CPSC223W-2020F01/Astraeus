7ef6b3bb7e212f0bfca1caa502b4fd400d657266
AaronLieb <aarnliebrmn@gmail.com>
2020-11-30 21:30:14 -0800

    Event protocol

---
+++ Astraeus/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  Astraeus
+//
+//  Created by Aaron Lieberman on 11/30/20.
+//
+
+import Foundation
+
+protocol Event {
+    var date : Date {get set}
+    var name : String {get set}
+}
