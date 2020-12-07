f250cc6b1579911163c671cc5c327f89767f0536
Taylor Noh <taylornoh99@gmail.com>
2020-12-06 20:25:59 -0800

    Updated Planets.json, Added an enumeration and sorting functions to PlanetInfo.swift, Added a button that shows a drop down menu to sort planets in PlanetList.swift

--- Astraeus/PlanetList.swift
+++ Astraeus/PlanetList.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct PlanetList: View {
+    @State private var sortSelection = sortValue.orbitDistance
+
     var body: some View {
         NavigationView {
             List(planetData!) { curPlanet in
@@ -16,12 +18,34 @@ struct PlanetList: View {
                 }
             }
             .navigationBarTitle(Text("Planets"))
+            .navigationBarItems(trailing: {
+                Menu {
+                    Button(action: { print("Hello there") }) {
+                        Picker(selection: $sortSelection, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                            Text("Orbit Distance").tag(sortValue.orbitDistance)
+                            Text("Radius").tag(sortValue.radius)
+                            Text("Volume").tag(sortValue.volume)
+                            Text("Mass").tag(sortValue.mass)
+                            Text("Density").tag(sortValue.density)
+                            Text("Gravity").tag(sortValue.gravity)
+                            Text("Rotation Period").tag(sortValue.rotationPeriod)
+                            Text("Orbit Period").tag(sortValue.orbitPeriod)
+                            Text("Average Orbit Velocity").tag(sortValue.avgOrbitVelocity)
+                            Text("Orbit Inclination").tag(sortValue.orbitInclination)
+                        }
+                    }
+                } label: {
+                    Image(systemName: "arrow.up.arrow.down")
+                }
+
+            }())
         }
     }
-    init()
-    {
+
+    init() {
         if planetData != nil {
-            print(planetData)
+            print(planetData!)
+            sortPlanetData(property: .orbitDistance)
         }
     }
 }
