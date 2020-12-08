b001653d691f660ae272ebb615bfc1c5c05b1948
Alex <tranalex7@gmail.com>
2020-12-08 02:26:46 -0800

    space Event stuff, RocketEvent example, testSort

--- Astraeus/Event.swift
+++ Astraeus/Event.swift
@@ -20,3 +20,17 @@ class TestEvent: Event {
     let description = "This is a test event."
     let type = "Test"
 }
+
+struct RocketEvent: Event {
+    var date: Date
+    var name: String
+    var description: String
+    var type: String
+    
+    init() {
+        date = Util.stringToDate("2020-12-06")!
+        name = "Falcon 9 • SpaceX CRS 21"
+        description = "A SpaceX Falcon 9 rocket will launch a Dragon 2 spacecraft on its first cargo resupply mission to the International Space Station. The flight is the 21st mission by SpaceX conducted under the Commercial Resupply Services contract with NASA. Delayed from Aug. 5, Oct. 30, Nov. 15, Nov. 22, and Dec. 2. Delayed from Dec. 5 by poor weather in booster recovery area."
+        type = "Rocket Launch"
+    }
+}
--- AstraeusTests/AstraeusTests.swift
+++ AstraeusTests/AstraeusTests.swift
@@ -29,5 +29,39 @@ class AstraeusTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testSorts() {
+        let cases = SortValue.all()
+        
+        for curSort in cases {
+            sortPlanetData(property: curSort)
+            for i in 0..<planetData!.count - 1 {
+                switch curSort {
+                    case .orbitDistance:
+                        XCTAssertLessThanOrEqual(planetData![i].orbitDistance, planetData![i + 1].orbitDistance)
+                    case .radius:
+                        XCTAssertLessThanOrEqual(planetData![i].radius, planetData![i + 1].radius)
+                    case .volume:
+                        XCTAssertLessThanOrEqual(planetData![i].volume, planetData![i + 1].volume)
+                    case .mass:
+                        XCTAssertEqual(Util.intsLessOrEqualTo(planetData![i].mass, planetData![i + 1].mass), true)
+                    case .density:
+                        XCTAssertLessThanOrEqual(planetData![i].density, planetData![i + 1].density)
+                    case .gravity:
+                        XCTAssertLessThanOrEqual(planetData![i].gravity, planetData![i + 1].gravity)
+                    case .rotationPeriod:
+                        XCTAssertLessThanOrEqual(planetData![i].rotationPeriod, planetData![i + 1].rotationPeriod)
+                    case .orbitPeriod:
+                        XCTAssertLessThanOrEqual(planetData![i].orbitPeriod, planetData![i + 1].orbitPeriod)
+                    case .avgOrbitVelocity:
+                        XCTAssertLessThanOrEqual(planetData![i].avgOrbitVelocity, planetData![i + 1].avgOrbitVelocity)
+                    case .orbitInclination:
+                        XCTAssertLessThanOrEqual(planetData![i].orbitInclination, planetData![i + 1].orbitInclination)
+                    case .surfaceTempRange:
+                        XCTAssertLessThanOrEqual(planetData![i].surfaceTempRange, planetData![i + 1].surfaceTempRange)
+                }
+            }
+        }
+    }
 
 }
